6350014e676c1a1aa41d2d2908f3e975a6e9a6ff
Ryan Duffy <ryan.duffy@lge.com>
2019-03-12 15:37:20 -0700

    change listener API to allow adapter
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- preset/webostv.js
+++ preset/webostv.js
@@ -1,11 +1,23 @@
+import {forKey} from '@enact/core/handle';
+
 import {config as moonstone} from './moonstone';
 import {config as ilib} from './ilib';
 import {config as webos, configure as conf} from './webos';
 
+const adapter = (keyName) => keyName ? {keyName} : null;
+
+const keys = ['red', 'blue', 'green', 'yellow', 'left', 'right', 'up', 'down'];
+
 const config = {
     ...moonstone,
     ...ilib,
-    ...webos
+    ...webos,
+    listeners: {
+        keydown: {
+            filter: (ev) => keys.find(n => forKey(n, ev)),
+            adapter: (ev) => adapter(keys.find(n => forKey(n, ev)))
+        }
+    }
 };
 
 const configure = (cfg) => {
