7045ceae8384f7c62df6add0d416e7b8dc3f486e
Ryan Duffy <ryan.duffy@lge.com>
2019-03-13 11:13:39 -0700

    memoize string mapper
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- preset/ilib.js
+++ preset/ilib.js
@@ -1,24 +1,37 @@
+import {memoize} from '@enact/core/util';
+import ilib from '@enact/i18n';
 import ResBundle from '@enact/i18n/ilib/lib/ResBundle';
 
 import {configure as conf} from '../index';
 
+const memoMapper = memoize((/* locale */) => {
+    // Retrieve the ResBundle to get access to the string map for the current locale.
+    const resBundle = new ResBundle();
+    const map = resBundle.getResObj();
+
+    // We have to convert the case since the text may be converted by UI components to a
+    // different case during render.
+    const keys = Object.keys(map).map(s => s.toUpperCase());
+
+    return (value) => keys[keys.indexOf(value.toUpperCase())];
+});
+
+const getMapper = () => memoMapper(ilib.getLocale());
+
 const config = {
     format: (msg) => {
-        // Retrieve the ResBundle to get access to the string map for the current locale.
-		const resBundle = new ResBundle();
-        const map = resBundle.getResObj();
+        const findValue = getMapper();
 
         // For every key (except type and time), attempt to delocalize the string.
-		Object.keys(msg).filter(key => key !== 'type' && key !== 'time').forEach(msgKey => {
-            // We have to convert the case since the text may be converted by UI components to a
-            // different case during render.
-			const upperValue = msg[msgKey].toUpperCase();
-			const value = Object.keys(map).find(key => upperValue === map[key].toUpperCase());
+        Object.keys(msg)
+            .filter(key => key !== 'type' && typeof msg[key] === 'string')
+            .forEach(msgKey => {
+                const value = findValue(msg[msgKey]);
 
-			if (value) {
-				msg[msgKey] = value;
-			}
-		});
+                if (value) {
+                    msg[msgKey] = value;
+                }
+            });
 
 		return msg;
 	}
